4c635ac609453d83a6f45260f38b7c68529c9906
Pratyush Prakash <geekrocks42@gmail.com>
2024-02-25 02:34:57 -0600

    fixed intake handoff

--- src/main/java/frc/robot/subsystems/Intake/IntakeHandoff.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeHandoff.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.Intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shooter.Shooter;
-import frc.robot.util.SteelTalonsLogger;
 
 public class IntakeHandoff extends Command {
     private Intake intake;
@@ -14,35 +13,28 @@ public class IntakeHandoff extends Command {
     @Override
     public void initialize() {
         intake.setPivotSetpoint(IntakeConstants.HANDOFF_POS);
-        SteelTalonsLogger.post("running outtake", false);
     }
 
     @Override
     public void execute() {
 
-        // SteelTalonsLogger.post("Timer 2", timer2.get());
         if (intake.atGoal(5.0) && Shooter.getInstance().pivotAtGoal(3.0)){
-            // System.err.println("intake outtaking");
-            // SteelTalonsLogger.post("runnig outtake", true);
             intake.setRollerSetpoint(IntakeConstants.INTAKE_SPEED_EJECTING);
         }
 
         if (Shooter.getInstance().loaded()) {
             intake.setPivotSetpoint(IntakeConstants.STOWED_POS);
-            // System.err.println("moving intake back");
         }
     }
 
     @Override
     public boolean isFinished() {
         return !intake.sensorCovered();
-        // return false;
     }
 
     @Override
     public void end(boolean interrupted) {
-        // intake.setRollerSetpoint(IntakeConstants.INTAKE_SPEED_HOLD);
-        SteelTalonsLogger.post("running outtake", false);
+        intake.setRollerSetpoint(IntakeConstants.INTAKE_SPEED_HOLD);
     }
 
 
