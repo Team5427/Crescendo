acffeff069a7f2768abfc8dc8977088e6928b39b
Saathvik Kamma <saathvikkamma@gmail.com>
2024-02-26 23:48:31 -0600

    Testing On Actual Repo

--- src/main/java/frc/robot/subsystems/Swerve/DrivetrainConstants.java
+++ src/main/java/frc/robot/subsystems/Swerve/DrivetrainConstants.java
@@ -12,7 +12,7 @@ import edu.wpi.first.math.util.Units;
 import frc.robot.util.STSmaxConfig;
 
 public class DrivetrainConstants {
-    public static final double WHEEL_DIAMETER_METERS = Units.inchesToMeters(3.88); //3.88
+    public static final double WHEEL_DIAMETER_METERS = Units.inchesToMeters(3.88); // 3.88
     public static final double TRACKWIDTH = Units.inchesToMeters(19.5);
     public static final double WHEELBASE = Units.inchesToMeters(19.5);
 
@@ -27,11 +27,12 @@ public class DrivetrainConstants {
     public static final double MAX_TRANSLATION_SPEED_M_S_TELEOP = Units.feetToMeters(16);
     public static final double MAX_ROTATION_SPEED_RAD_S_TELEOP = 4 * Math.PI;
 
-    public static final double MAX_PHYSICAL_SPEED_M_S = (5676 * (14.0 / 50.0) * (28.0 / 16.0) * (15.0 / 45.0) * WHEEL_DIAMETER_METERS * Math.PI) / (60.0);
+    public static final double MAX_PHYSICAL_SPEED_M_S = (5676 * (14.0 / 50.0) * (28.0 / 16.0) * (15.0 / 45.0)
+            * WHEEL_DIAMETER_METERS * Math.PI) / (60.0);
     public static final double MAX_ACCEL = MAX_PHYSICAL_SPEED_M_S * 5;
     public static final double THRESHOLD_STOPPING_M_S_COMPETITION = 0.07;
     public static final double THRESHOLD_STOPPING_M_S_TUNING = 0.5;
-
+    // GOOD STUFF YALL WE GOT THIS
     public static final int FRONT_LEFT_CANCODER_ID = 12;
     public static final int FRONT_RIGHT_CANCODER_ID = 13;
     public static final int BACK_LEFT_CANCODER_ID = 14;
@@ -75,7 +76,6 @@ public class DrivetrainConstants {
         FRONT_LEFT_DRIVE.inverted = false;
         BACK_LEFT_DRIVE.inverted = false;
 
-
         FRONT_LEFT_STEER.id = 6;
         FRONT_RIGHT_STEER.id = 4;
         BACK_LEFT_STEER.id = 10;
@@ -83,34 +83,35 @@ public class DrivetrainConstants {
     }
 
     // public static void configureDriveTalon(TalonFX motor) {
-    //     Slot0Configs velConstants = new Slot0Configs();
-    //     velConstants.kP = 0.0; //FIXME
-    //     velConstants.kS = 0.5; //FIXME
-    //     velConstants.kV = 12 / (MAX_PHYSICAL_SPEED_M_S);
-    //     motor.getConfigurator().apply(velConstants);
+    // Slot0Configs velConstants = new Slot0Configs();
+    // velConstants.kP = 0.0; //FIXME
+    // velConstants.kS = 0.5; //FIXME
+    // velConstants.kV = 12 / (MAX_PHYSICAL_SPEED_M_S);
+    // motor.getConfigurator().apply(velConstants);
 
-    //     FeedbackConfigs feedbackConfigs = new FeedbackConfigs();
-    //     feedbackConfigs.SensorToMechanismRatio = (14.0 / 50.0) * (28.0 / 16.0) * (15.0 / 45.0) * WHEEL_DIAMETER_METERS * Math.PI;
-    //     motor.getConfigurator().apply(feedbackConfigs);
+    // FeedbackConfigs feedbackConfigs = new FeedbackConfigs();
+    // feedbackConfigs.SensorToMechanismRatio = (14.0 / 50.0) * (28.0 / 16.0) *
+    // (15.0 / 45.0) * WHEEL_DIAMETER_METERS * Math.PI;
+    // motor.getConfigurator().apply(feedbackConfigs);
 
-    //     ClosedLoopRampsConfigs rampConfigs = new ClosedLoopRampsConfigs();
-    //     rampConfigs.VoltageClosedLoopRampPeriod = 0.2; //secs it takes to ramp to max
-    //     motor.getConfigurator().apply(rampConfigs);
+    // ClosedLoopRampsConfigs rampConfigs = new ClosedLoopRampsConfigs();
+    // rampConfigs.VoltageClosedLoopRampPeriod = 0.2; //secs it takes to ramp to max
+    // motor.getConfigurator().apply(rampConfigs);
 
-    //     CurrentLimitsConfigs currConfigs = new CurrentLimitsConfigs();
-    //     currConfigs.StatorCurrentLimitEnable = true;
-    //     currConfigs.SupplyCurrentLimitEnable = true;
-    //     currConfigs.StatorCurrentLimit = 60;
-    //     currConfigs.SupplyCurrentLimit = 60;
-    //     currConfigs.SupplyCurrentThreshold = 80;
-    //     currConfigs.SupplyTimeThreshold = 1.5;
-    //     motor.getConfigurator().apply(currConfigs);
+    // CurrentLimitsConfigs currConfigs = new CurrentLimitsConfigs();
+    // currConfigs.StatorCurrentLimitEnable = true;
+    // currConfigs.SupplyCurrentLimitEnable = true;
+    // currConfigs.StatorCurrentLimit = 60;
+    // currConfigs.SupplyCurrentLimit = 60;
+    // currConfigs.SupplyCurrentThreshold = 80;
+    // currConfigs.SupplyTimeThreshold = 1.5;
+    // motor.getConfigurator().apply(currConfigs);
 
-    //     motor.setNeutralMode(NeutralModeValue.Brake);
+    // motor.setNeutralMode(NeutralModeValue.Brake);
     // }
 
     public static STSmaxConfig configureDriveNeo(STSmaxConfig config) {
-        
+
         config.kP = 0.02;
         config.kFF = 1 / DrivetrainConstants.MAX_PHYSICAL_SPEED_M_S;
         config.kD = 0.0;
@@ -127,10 +128,10 @@ public class DrivetrainConstants {
 
     public static STSmaxConfig configureSteerNeo(STSmaxConfig config) {
         config.currentLimit = 20;
-        config.gearing = (1.0 / (150.0/7.0));
+        config.gearing = (1.0 / (150.0 / 7.0));
         config.isRotational = true;
         config.kP = 7.0;
-        config.kD = 0.15; //1.6
+        config.kD = 0.15; // 1.6
         config.maxVel = (5676 * config.gearing * 2 * Math.PI) / (60.0);
         config.maxAccel = config.maxVel * 1000;
         config.inverted = true;
@@ -146,5 +147,5 @@ public class DrivetrainConstants {
 
         encoder.getConfigurator().apply(config);
     }
-    
+
 }
